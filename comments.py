bb49b8b26028aaff89b0b6f853985a19fdf214d3
svtcore <67793906+svtcore@users.noreply.github.com>
2024-08-03 20:16:52 +0300

    - added support for private channels - changed input type of channels

--- comments.py
+++ comments.py
@@ -1,5 +1,4 @@
 import datetime
-from os import close
 from pyrogram import Client
 from pyrogram.raw import functions
 import time
@@ -8,7 +7,6 @@ from pyrogram.errors import BadRequest, FloodWait
 from datetime import datetime
 import codecs
 import math
-import os
 
 
 class Comments:
@@ -97,7 +95,7 @@ class Comments:
     Iterate message part, check if comment from target user then concatenate it with other comment data
     Processing only text messages
     '''
-    def formatResultText(self, result, channel_title, channel_username, channel_message_id):
+    def formatResultText(self, result, channel_link, channel_title, channel_username, channel_message_id, channel_private_status):
         try:
             result_text = ""
             for j in range(0, len(result.messages)):
@@ -119,6 +117,19 @@ class Comments:
                         user_message = (result.messages[j].message).replace('\n', '.')
                         #double quotes crashing formatting on the export file so replace it to another type
                         user_message = (result.messages[j].message).replace('"', '`')
+                    else:
+                        try:
+                            #other types of messsages voices, gifs, video
+                            if hasattr(result.messages[j].media, 'document'):
+                                user_message = str(result.messages[j].media.document.mime_type)
+                            else:
+                                user_message = "UNKNOWN_TYPE_OF_MESSAGE"
+                        except:
+                            print("PROBLEM_WITH_RECONGNIZING_NO_TEXT_MESSAGE")
+                    if (channel_private_status):
+                        result_text = (result_text + str(str_date) + ',' + str(channel_title) + ',' + str(channel_link) + ',' + '"'+str(user_message).strip(
+                        ) + '"'+','+'https://t.me/c/' + str(result.messages[j].peer_id.channel_id) + '/' + str(result.messages[j].id) + '?thread=' + str(result.messages[j].reply_to.reply_to_msg_id)).strip() + '\n'
+                    else:
                         result_text = (result_text + str(str_date) + ',' + str(channel_title) + ',' + str(channel_username) + ',' + '"'+str(user_message).strip(
                         ) + '"'+','+'https://t.me/' + str(channel_username) + '/' + str(channel_message_id) + '?comment=' + str(result.messages[j].id)).strip() + '\n'
             if result_text.strip() == "":
@@ -127,6 +138,38 @@ class Comments:
                 return result_text
         except NameError:
             return NameError
+
+    '''
+    Check channel state private or public
+    '''
+    def checkPrivateChannel(self, channel_id):
+        try:
+            chat = self.app.get_chat(channel_id)
+            #if channel has username then it's public
+            if chat.username:
+                return False
+            else:
+                return True
+        except NameError:
+            print(NameError)
+
+    '''
+    Get channel id based on link
+    '''
+    def getChannelId(self, channel_link):
+        try:
+            #private channel case
+            if (channel_link.startswith("https://t.me/+")) or channel_link.startswith("http://t.me/+") or channel_link.startswith("t.me/+"):
+                chat = self.app.get_chat(channel_link)
+            else:
+                #public channel, cut only username
+                username = channel_link.split("/")[-1]
+                chat = self.app.get_chat(username)
+            chat_id = chat.id
+            return chat_id
+        except NameError:
+            print(NameError)
+
     '''
     Main methods call methods for work, get basic data for work, process it and write to file
     '''
@@ -135,15 +178,19 @@ class Comments:
             self.loadChannels()
             self.auth()
             for channel in self.channels:
+                channel_general_id = self.getChannelId(channel)
+                channel_private_status = self.checkPrivateChannel(channel_general_id)
                 target_message_history = list(self.app.get_chat_history(
-                    channel, limit=self.POSTS_LIMIT))
+                    channel_general_id, limit=self.POSTS_LIMIT))
                 for i in range(0, len(target_message_history)):
                     try:
                         channel_id = target_message_history[i].sender_chat.id
                         channel_title = target_message_history[i].sender_chat.title
                         channel_username = target_message_history[i].sender_chat.username
                         channel_message_id = target_message_history[i].id
-                        print("Processing " + "[" + str(int(i + 1)) + "/" + str(len(target_message_history)) + "] " + channel_username + "/" + str(channel_message_id))
+                        if (channel_username == None):
+                            channel_username = channel_title
+                        print("Processing " + "[" + str(int(i + 1)) + "/" + str(len(target_message_history)) + "] " + str(channel_username) + "/" + str(channel_message_id))
                         # Getting data about comments in post
                         result = self.getReplies(channel_id, channel_message_id, 0)
                         offset = 0
@@ -152,7 +199,7 @@ class Comments:
                         for k in range(0, math.ceil(int(result.count)/100)):
                             result = self.getReplies(channel_id, channel_message_id, offset)
                             offset = offset + 100
-                            result_text = self.formatResultText(result, channel_title, channel_username, channel_message_id)
+                            result_text = self.formatResultText(result, channel, channel_title, channel_username, channel_message_id, channel_private_status)
                             if (result_text != None):
                                 self.writeToFile(result_text)
                             time.sleep(2)
